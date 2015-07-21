712a29bb213bfc52f6cc43059a1a335654ff2ec4
Zohreh <zohreh.alavi@gmail.com>
2015-07-21 16:26:30 -0400

    initial commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/baxter/ros_ws/src/baxter_srv/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/baxter/ros_ws/src/baxter_srv/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/baxter/ros_ws/src/baxter_srv/build/devel/lib:/home/baxter/ros_ws/src/baxter_srv/build/devel/lib/x86_64-linux-gnu:/home/baxter/ros_ws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/baxter/ros_ws/devel/lib:/opt/ros/indigo/lib:/usr/local/lib"
+export PATH="/home/baxter/ros_ws/src/baxter_srv/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/baxter/ros_ws/src/baxter_srv/build/devel/lib/pkgconfig:/home/baxter/ros_ws/src/baxter_srv/build/devel/lib/x86_64-linux-gnu/pkgconfig:/home/baxter/ros_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/baxter/ros_ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PYTHONPATH="/home/baxter/ros_ws/src/baxter_srv/build/devel/lib/python2.7/dist-packages:/home/baxter/ros_ws/devel/lib/python2.7/dist-packages:/opt/ros/indigo/lib/python2.7/dist-packages:~/ros_ws/src:~/ros_ws/src"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/baxter/ros_ws/src/baxter_srv/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/baxter/ros_ws/src/baxter_srv:$ROS_PACKAGE_PATH"
---
+++ build/gtest/CMakeFiles/gtest_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/gtest_main.dir/src/gtest_main.cc.o"
+  "/home/baxter/ros_ws/src/baxter_srv/lib/libgtest_main.pdb"
+  "/home/baxter/ros_ws/src/baxter_srv/lib/libgtest_main.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/gtest_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
